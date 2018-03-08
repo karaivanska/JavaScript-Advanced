4378ae7b209d3686799ad3943e9b4603dc79b114
karaivanska <karaivanska@gmail.com>
2018-03-08 11:36:36 +0200

    Adding exercise.

--- 3.1 Advanced-functions-Lab/05.nextArticle.js
+++
@@ -1,7 +0,0 @@
-function getArticleGenerator(articles) {
-   return function () {
-       if(articles.length !== 0){
-           $('#content').append($(`<article>${articles.shift()}</article>`));
-       }
-   }
-}
